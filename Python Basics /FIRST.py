06ed77068b8ff18323d368e49a8f40ef5e98ed67
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-09 10:00:33 +0000

    updated the file and writed the program to mulktiplying the two elements

--- Python Basics /FIRST.py
+++ Python Basics /FIRST.py
@@ -1,1 +1,6 @@
-print("hello world !")
+# variables  and data type example in python
+a = 12
+b = 23
+c = a*b
+print("the output will be : ",c) 
+#ya it was not an example of the data type itself but i was revising the things that how the two datatype get multiplied so here it was 
